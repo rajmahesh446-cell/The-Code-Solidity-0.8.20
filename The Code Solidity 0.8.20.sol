e6ba8ffacfe2b594404092c34941875ff5239bb7
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-02-05 11:34:12 +0530

    Update The Code Solidity 0.8.20.sol

--- The Code Solidity 0.8.20.sol
+++ The Code Solidity 0.8.20.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
